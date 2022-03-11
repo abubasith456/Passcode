a88acf2c5544ba8a56ae142fcd35becae8b6c2ec
abubasith456 <abubasith456@gmail.com>
2022-03-11 14:26:02 +0530

    Pick from gallery added

--- app/src/main/java/com/example/passcode/activity/DashboardActivity.java
+++ app/src/main/java/com/example/passcode/activity/DashboardActivity.java
@@ -1,6 +1,7 @@
 package com.example.passcode.activity;
 
 import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.view.View.GONE;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -8,8 +9,10 @@ import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.GridLayoutManager;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -33,8 +36,12 @@ public class DashboardActivity extends AppCompatActivity {
     private static final int PERMISSION_REQUEST_CODE = 200;
     public static int PICK_IMAGE = 100;
     public static int PICK_VIDEO = 101;
+    private String[] cameraPermissions;
+    private static final int IMAGE_PICK_CAMERA_CODE = 500;
+    private static final int CAMERA_REQUEST_CODE = 300;
     ActivityDashboardBinding activityDashboardBinding;
     public ArrayList<String> paths = new ArrayList<>();
+    private Uri imageUri;
 
 
     @Override
@@ -42,6 +49,7 @@ public class DashboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_dashboard);
         activityDashboardBinding = DataBindingUtil.setContentView(this, R.layout.activity_dashboard);
+        cameraPermissions = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
         requestPermissions();
 
         activityDashboardBinding.buttonPickFromGallery.setOnClickListener(new View.OnClickListener() {
@@ -65,11 +73,23 @@ public class DashboardActivity extends AppCompatActivity {
             }
         });
 
+        activityDashboardBinding.buttonOpenCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkCameraPermission()) {
+                    pickFromCamera();
+                } else {
+                    requestCameraPermission();
+                }
+            }
+        });
+
 
     }
 
     public void showGallery() {
 //        GalleryView.show(DashboardActivity.this, paths);
+        Log.e("Paths", String.valueOf(paths));
         GalleryView.show(this, paths, 0, new ActionCallback() {
 
             @Override
@@ -97,9 +117,19 @@ public class DashboardActivity extends AppCompatActivity {
         return result == PackageManager.PERMISSION_GRANTED;
     }
 
+    private boolean checkCameraPermission() {
+        boolean resultCamera = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) ==
+                (PackageManager.PERMISSION_GRANTED);
+        return resultCamera;
+    }
+
     private void requestPermission() {
         //on below line we are requesting the rea external storage permissions.
         ActivityCompat.requestPermissions(this, new String[]{READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+    }
+
+    private void requestCameraPermission() {
+        ActivityCompat.requestPermissions(this, cameraPermissions, CAMERA_REQUEST_CODE);
     }
 
     @Override
@@ -118,9 +148,16 @@ public class DashboardActivity extends AppCompatActivity {
             if (data != null) {
                 Uri contentURI = data.getData();
                 String selectedImagePath = getImagePath(contentURI);
-                Log.d("path", selectedImagePath);
+                Log.e("path", selectedImagePath);
                 paths.add(selectedImagePath);
             }
+        } else if (requestCode == IMAGE_PICK_CAMERA_CODE && resultCode == RESULT_OK) {
+            if (imageUri != null) {
+                String path = getImagePath(imageUri);
+                paths.add(path);
+                Log.e("Done in path", path);
+            }
+
         }
     }
 
@@ -143,7 +180,7 @@ public class DashboardActivity extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         switch (requestCode) {
             // we are checking the permission code.
-            case PERMISSION_REQUEST_CODE:
+            case PERMISSION_REQUEST_CODE: {
                 // in this case we are checking if the permissions are accepted or not.
                 if (grantResults.length > 0) {
                     boolean storageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
@@ -156,8 +193,33 @@ public class DashboardActivity extends AppCompatActivity {
                         Toast.makeText(this, "Permissions denied, Permissions are required to use the app..", Toast.LENGTH_SHORT).show();
                     }
                 }
-                break;
+            }
+            break;
+            case CAMERA_REQUEST_CODE: {
+                if (grantResults.length > 0) {
+                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    if (cameraAccepted) {
+                        pickFromCamera();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Camera and storage permission required..", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+            break;
+
         }
+    }
+
+    private void pickFromCamera() {
+        //using media to pic high quality image
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.Images.Media.TITLE, "Temp_image_title");
+        contentValues.put(MediaStore.Images.Media.DESCRIPTION, "Temp_image_description");
+        imageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+        Intent pickCameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        pickCameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(pickCameraIntent, IMAGE_PICK_CAMERA_CODE);
+        Log.e("After pick ", String.valueOf(imageUri));
     }
 
     @Override
